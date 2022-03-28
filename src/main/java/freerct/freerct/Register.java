412fffefba31f23e3ee16cb26417367e8bb96d9f
Benedikt Straub <benedikt-straub@web.de>
2022-03-28 14:17:48 +0200

    Fix token timeout

--- src/main/java/freerct/freerct/Register.java
+++ src/main/java/freerct/freerct/Register.java
@@ -107,7 +107,7 @@ public class Register {
 
 			final String randomToken = SecurityManager.generateRandomToken();
 			Calendar tokenExpiry = Calendar.getInstance();
-			tokenExpiry.roll(Calendar.DAY_OF_MONTH, 7);  // Keep the token valid for 7 days.
+			tokenExpiry.roll(Calendar.DATE, 7);  // Keep the token valid for 7 days.
 
 			// TODO run a scheduled job that frequently purges stale tokens from the database
 
